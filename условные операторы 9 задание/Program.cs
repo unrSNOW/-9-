786ca04279886e0e887e9dd5d93f87be6d483aaa
Asus <Asus@LAPTOP-4QTPOURE>
2023-10-16 18:00:53 +0300

    Добавьте файлы проекта.

---
+++ условные операторы 9 задание/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите трехзначное число x:");
+int x = int.Parse(Console.ReadLine());
+int sum;
+int proz;
+sum = ((x % 10) + (x/10 %10) + (x / 100));
+if (sum >= 10 && sum <= 99) Console.WriteLine(sum);
+else Console.WriteLine("сумма цифр числа не является двухзначной");
+proz = ((x % 10) * (x / 10 %10) * (x / 100));
+if (proz >= 100 && proz <= 999) Console.WriteLine(proz);
+else Console.WriteLine("произведение чисел не является трехзначным");
+
+
